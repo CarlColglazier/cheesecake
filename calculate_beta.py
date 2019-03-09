153467f90d28d884644bf5873e7fa5d2f2cfd85f
Carl Colglazier <carl@carlcolglazier.com>
2019-03-09 18:16:55 -0500

    Simplify/speed up script.

--- calculate_beta.py
+++ calculate_beta.py
@@ -1,9 +1,16 @@
 from cheesecake.models import *
 import itertools
 from scipy.stats import beta
+from sqlalchemy.orm import joinedload
+import pandas as pd
 
-events = Event.query.filter(Event.year == 2019).filter(Event.week == 0).all()
-matches = list(itertools.chain.from_iterable([x.matches for x in events]))
+#events = Event.query.filter(Event.year == 2019).filter(Event.week == 0).all()
+matches = Match.query.options(joinload(Match.event_key)).filter(
+    Event.year == 2019
+).filter(
+    Event.week == 0
+).all()
+#matches = list(itertools.chain.from_iterable([x.matches for x in events]))
 matches = [x for x in matches if x.winning_alliance != "" and x.comp_level == "qm"]
 colors = ['red', 'blue']
 teams = {}
@@ -11,7 +18,8 @@ blank = {"sucess": 0, "attempts": 0}
 for match in matches:
     alliances = match.get_alliances()
     for color in colors:
-        sucess = match.score_breakdown[color]["habDockingRankingPoint"]
+        #sucess = match.score_breakdown[color]["habDockingRankingPoint"]
+        sucess = match.score_breakdown[color]["completeRocketRankingPoint"]
         for team in alliances[color].team_keys:
             key = team.key
             if key not in teams:
@@ -22,4 +30,7 @@ for match in matches:
 
 
 data = pd.DataFrame.from_dict(teams, orient='index')
-print(beta.fit(data["sucess"]/data["attempts"]))
+print(beta.fit(data["sucess"]/data["attempts"], loc=0, scale=1))
+
+# a=0.7229 b=2.4517
+# a=0.2749 b=62.2755
