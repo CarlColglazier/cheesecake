56f7843663f74891cd5662844c638aebe18e4e15
Carl Colglazier <carl@carlcolglazier.com>
2019-07-26 11:40:38 -0400

    Add nuxt.js frontend.

---
+++ frontend/layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app>
+      <!-- -->
+    </v-navigation-drawer>
+    <v-app-bar app>
+      <!-- -->
+    </v-app-bar>
+    <!-- Sizes your content based upon application components -->
+    <v-content>
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+        <router-view />
+      </v-container>
+    </v-content>
+    <v-footer app>
+      <!-- -->
+      Footer.
+    </v-footer>
+  </v-app>
+</template>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex sx12 sm8 md6>
+      <v-card>
+        <v-card-title>Hello</v-card-title>
+        <v-card-text>
+          How are you?
+        </v-card-text>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  layout: 'default'
+}
+</script>
