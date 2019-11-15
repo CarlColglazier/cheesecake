beee8d83ec83ce47afd4e5d0f5e642285fb0dc5f
Carl Colglazier <carl@carlcolglazier.com>
2019-11-15 15:02:33 -0500

    Calculate URL for prod server.

--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
-let base = process.server ? 'http://backend:8080/' : 'http://localhost:8080/'
-if (process.env.ENVIRONMENT === 'production') {
-	base = 'https://cheesecake.live/'
+let base = 'http://localhost:8080/'
+if (process.server) {
+	base = 'http://backend:8080/'
+} else if (process.env.ENVIRONMENT === 'production') {
+	base = 'https://cheesecake.live/api/'
 }
 
 export default axios.create({
