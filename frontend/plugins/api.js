cfdb9136b9f2da5d18af63491785789143124fc0
Carl Colglazier <carl@carlcolglazier.com>
2019-12-22 10:31:25 -0500

    Use NODE_ENV.

--- frontend/plugins/api.js
+++ frontend/plugins/api.js
@@ -3,7 +3,7 @@ import Vue from 'vue'
 let url;
 if (process.server) {
     url = 'http://backend:8080'
-} else if (process.env.ENVIRONMENT === 'production') {
+} else if (process.env.NODE_ENV === 'production') {
     url = 'https://cheesecake.live/api/';
 } else {
     url = 'http://localhost:8080';
