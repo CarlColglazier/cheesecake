10d2387c77e69f78c6b57ebedaad22b7a1f6a1a5
Carl Colglazier <carl@carlcolglazier.com>
2019-12-23 17:36:44 -0500

    Just return a blank JSON resp.

--- backend/routes.go
+++ backend/routes.go
@@ -23,7 +23,7 @@ func runServer(config Config) {
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	fmt.Fprintln(w, `{status: "good 1"}`)
+	fmt.Fprintln(w, `{}`)
 }
 
 func (config *Config) GetEventMatchesReq(w http.ResponseWriter, r *http.Request) {
