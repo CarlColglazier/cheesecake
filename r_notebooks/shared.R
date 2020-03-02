6c70edbfc037ca618bfa1bfc4db1215217c6f2f2
Carl Colglazier <carl@carlcolglazier.com>
2020-03-02 15:40:27 -0500

    Calculate tuning for 2020.

--- r_notebooks/shared.R
+++ r_notebooks/shared.R
@@ -1,10 +1,17 @@
 library(DBI)
 library(tidyverse)
 
-con <- DBI::dbConnect(odbc::odbc(),
-                      Driver   = "PostgreSQL Unicode",
-                      Server   = "localhost",
-                      Database = "cheesecake",
-                      UID      = "cheese",
-                      PWD      = "cheesepass4279",
-                      Port     = 5432)
+#con <- DBI::dbConnect(odbc::odbc(),
+#                      Driver   = "PostgreSQL Unicode",
+#                      Server   = "localhost",
+#                      Database = "cheesecake",
+#                      UID      = "cheese",
+#                      PWD      = "cheesepass4279",
+#                      Port     = 5432)
+
+con <- dbConnect(RPostgres::Postgres(),
+                 dbname = 'cheesecake', 
+                 host = 'localhost', # i.e. 'ec2-54-83-201-96.compute-1.amazonaws.com'
+                 port = 5432, # or any other port specified by your DBA
+                 user = 'cheese',
+                 password = 'cheesepass4279')
