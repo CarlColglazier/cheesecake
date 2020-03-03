3a7f1e3773eab3b9dd3797cb7d5b52ebc2360cba
Carl Colglazier <carl@carlcolglazier.com>
2020-03-03 10:31:14 -0500

    DB settings.

--- backend/db.go
+++ backend/db.go
@@ -3,19 +3,21 @@ package main
 import (
 	"context"
 	"fmt"
+	"time"
 
-	//"time"
-
+	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 func Connect(host, applicationName string) (*pgxpool.Pool, error) {
-	connString := "postgres://cheese:cheesepass4279@" + host + ":5432/" + applicationName + "?pool_max_conns=16&pool_min_conns=1"
+	connString := "postgres://cheese:cheesepass4279@" + host + ":5432/" + applicationName
 	config, err := pgxpool.ParseConfig(connString)
 	if err != nil {
 		return nil, err
 	}
 	fmt.Println(connString)
-	//config.MaxConnLifetime = time.Duration(2 * 1000000000)
+	config.MaxConns = 5
+	config.MaxConnLifetime = 10 * time.Second
+	config.ConnConfig.LogLevel = pgx.LogLevelDebug
 	return pgxpool.ConnectConfig(context.Background(), config)
 }
