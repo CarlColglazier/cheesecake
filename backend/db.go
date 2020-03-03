e847aa7887beb6eb203e59ad084c70c1b3904846
Carl Colglazier <carl@carlcolglazier.com>
2020-03-03 10:44:44 -0500

    DB config (test)

--- backend/db.go
+++ backend/db.go
@@ -16,8 +16,10 @@ func Connect(host, applicationName string) (*pgxpool.Pool, error) {
 		return nil, err
 	}
 	fmt.Println(connString)
+	config.MinConns = 1
 	config.MaxConns = 5
 	config.MaxConnLifetime = 10 * time.Second
+	config.HealthCheckPeriod = 15 * time.Second
 	config.ConnConfig.LogLevel = pgx.LogLevelDebug
 	return pgxpool.ConnectConfig(context.Background(), config)
 }
