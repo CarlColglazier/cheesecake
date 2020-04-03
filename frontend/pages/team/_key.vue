1d047481504703ec4e490c72ede494a0eacc7235
Carl Colglazier <carl@carlcolglazier.com>
2020-04-03 18:41:49 -0400

    Frontend page for teams.

---
+++ frontend/pages/team/_key.vue
@@ -0,0 +1,49 @@
+<template>
+	<content>
+		<h1>{{ $nuxt.$route.params.key }}</h1>
+		<b-table-simple>
+			<b-thead>
+				<b-tr>
+					<b-th>Key</b-th>
+					<b-th>Auto</b-th>
+					<b-th>Tele Cell</b-th>
+					<b-th>Endgame</b-th>
+				</b-tr>
+			</b-thead>
+			<b-tbody v-for="d in matches" :key="d.match.key">
+				<b-tr>
+					<b-td>{{ d.match.key }}</b-td>
+					<b-td>{{ d.match.score_breakdown[get_color(d)]['autoPoints'] }}</b-td>
+					<b-td>{{ d.match.score_breakdown[get_color(d)]['teleopCellPoints'] }}</b-td>
+					<b-td>{{ d.match.score_breakdown[get_color(d)]['endgamePoints'] }}</b-td>
+				</b-tr>
+			</b-tbody>
+		</b-table-simple>
+	</content>
+</template>
+
+<script>
+function get_color(d) {
+	return Object.keys(d.alliances)[0];
+}
+
+export default {
+	layout: 'default',
+	async asyncData(context) {
+		try {
+			const dataf = await context.app.fetch(`/team/${context.params.key}/2020`)
+			const data = await dataf.json()
+			console.log(data);
+			return { matches: data }
+		} catch (e) {
+			console.error(e)
+			return { matches: [] }
+		}
+	},
+	components: {
+	},
+	methods: {
+		get_color: get_color
+	}
+}
+</script>
