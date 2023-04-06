c3dda6e2d1c4805ed0d66aca862478b6f570452f
Carl Colglazier <carl@carlcolglazier.com>
2023-04-06 12:32:21 -0500

    Filter by week.

--- model/python/2023.py
+++ model/python/2023.py
@@ -6,6 +6,8 @@ import os
 from pathlib import Path
 import json
 import hashlib
+import argparse
+
 load_dotenv()
 
 API = "https://www.thebluealliance.com/api/v3"
@@ -59,6 +61,10 @@ def get_all_events_year(year):
 	url = f"events/{year}"
 	j, _ = run_query(url)
 	j = filter(lambda x: x['event_type'] <= 6, j)
+	return list(j)
+	#return list(map(lambda x: x['key'], j))
+
+def to_event_keys(j):
 	return list(map(lambda x: x['key'], j))
 
 def get_event(key):
@@ -137,34 +143,48 @@ def proc_match(m):
 			g.append(r)
 	return g
 
-event_keys = get_all_events_year(2023)
-for event_key in event_keys:
-	event = get_event(event_key)
-	matches, should_run = get_matches(event)
-	if should_run is False:
-		continue
-	if matches is None:
-		continue
-	if len(matches) == 0:
-		continue
-	m = list(map(proc_schedule, matches))
-	df = pd.DataFrame(m, columns=[
-		'key', 'comp_level', 'match_number', 'time', 'red_teams', 'blue_teams', 'red_score', 'blue_score'
-	])
-	if df.shape[0] > 0:
-		df.to_feather(f"../data/schedules/{event_key}.feather")
-	data = list(itertools.chain.from_iterable(map(proc_match, matches)))
-	df = pd.DataFrame(data, columns=[
-		'event', 'week', 'event_type', 'key', 'alliance', 'comp_level',
-		'match_number', 'time', 'team', 'score', 'winner',
-		'mobility', 'auto_charge', 'auto_charge_points',
-		'auto_countT', 'auto_countM', 'auto_countB',
-		'teleop_countT', 'teleop_countM', 'teleop_countB',
-		'endgame_charge', 'endGameBridgeState',
-		'activation', 'sustainability'
-	])
-	if df.shape[0] > 0:
-		df.to_feather(f"../data/breakdowns/{event_key}.feather")
 
-with open(Path(header_file_loc), "w") as f:
-	json.dump(header_cache, f)
+def run(week=None):
+	events = get_all_events_year(2023)
+	if week is not None:
+		events = list(filter(lambda x: x['week'] == week, events))
+	event_keys = to_event_keys(events)
+	for event_key in event_keys:
+		event = get_event(event_key)
+		matches, should_run = get_matches(event)
+		if should_run is False:
+			continue
+		if matches is None:
+			continue
+		if len(matches) == 0:
+			continue
+		m = list(map(proc_schedule, matches))
+		df = pd.DataFrame(m, columns=[
+			'key', 'comp_level', 'match_number', 'time', 'red_teams', 'blue_teams', 'red_score', 'blue_score'
+		])
+		if df.shape[0] > 0:
+			df.to_feather(f"../data/schedules/{event_key}.feather")
+		data = list(itertools.chain.from_iterable(map(proc_match, matches)))
+		df = pd.DataFrame(data, columns=[
+			'event', 'week', 'event_type', 'key', 'alliance', 'comp_level',
+			'match_number', 'time', 'team', 'score', 'winner',
+			'mobility', 'auto_charge', 'auto_charge_points',
+			'auto_countT', 'auto_countM', 'auto_countB',
+			'teleop_countT', 'teleop_countM', 'teleop_countB',
+			'endgame_charge', 'endGameBridgeState',
+			'activation', 'sustainability'
+		])
+		if df.shape[0] > 0:
+			df.to_feather(f"../data/breakdowns/{event_key}.feather")
+
+	with open(Path(header_file_loc), "w") as f:
+		json.dump(header_cache, f)
+
+def get_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--week", type=int, default=None)
+	return parser.parse_args()
+
+if __name__ == "__main__":
+	args = get_args()
+	run(week=args.week)
